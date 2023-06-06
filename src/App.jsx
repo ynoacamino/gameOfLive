f1fa3835cdf3b7913fe28f21af92a4b991035279
Diegoo11 <ynoacamino@gmail.com>
2023-06-05 21:15:02 -0500

    function for arrays 2d and respondive table

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react-hooks/exhaustive-deps */
 import {
   useState, React, useEffect, useRef,
@@ -5,9 +6,17 @@ import {
 import './App.css';
 
 function App() {
+  const createArrays = (num) => {
+    const elArray = [];
+    for (let i = 0; i < num; i += 1) {
+      elArray.push(Array(num).fill(0));
+    }
+    return (elArray);
+  };
   const [count, setCount] = useState(0);
   const [boxes, setBoxes] = useState(100);
   const numBoxes = useRef();
+  const [tablero, setTablero] = useState(createArrays(50));
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -17,7 +26,16 @@ function App() {
   }, []);
 
   const listenInput = () => {
-    console.log(numBoxes.current.e.target);
+    setBoxes(numBoxes.current.value);
+    console.log('hola');
+  };
+
+  const changeLIve = (live, x, y) => {
+    setTablero((table) => {
+      const newTable = table;
+      newTable[x][y] = 1;
+      return [...newTable];
+    });
   };
 
   return (
@@ -35,6 +53,19 @@ function App() {
       <button type="button">
         {boxes}
       </button>
+      <div className="tablero">
+        {tablero.map((n, x) => (
+          <div className="column">
+            {n.map((live, y) => (
+              // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+              <div
+                onClick={() => changeLIve(live, x, y)}
+                className={`box ${live === 1 ? 'live' : 'dead'}`}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
