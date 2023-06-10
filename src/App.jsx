33ee20ec3c9695518e233120dd74d0d0da3c5e36
Diegoo11 <ynoacamino@gmail.com>
2023-06-10 15:41:54 -0500

    new modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,46 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react-hooks/exhaustive-deps */
 import {
-  useState, React, useEffect, useRef,
+  useState, React, useRef,
 } from 'react';
 import './App.css';
-import { createArrays, law } from './test/util';
+import { createArrays } from './test/util';
+import TableSTOP from './TableSTOP/TableSTOP';
+import TableLive from './TableLIVE/TableLIVE';
+import Options from './Options/Options';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   const [boxes, setBoxes] = useState(100);
-  const numBoxes = useRef();
   const [tablero, setTablero] = useState(createArrays(boxes));
+  const [live, setLive] = useState(false);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCount((cuenta) => cuenta + 1);
-      setTablero((table) => {
-        const newTable = createArrays(boxes);
-        table.forEach((col, x) => {
-          col.forEach((cel, y) => {
-            newTable[x][y] = law(table, x, y, cel);
-          });
-        });
-        return [...newTable];
-      });
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-
-  const listenInput = () => {
-    setBoxes(Number(numBoxes.current.value));
-    setTablero(createArrays(Number(numBoxes.current.value)));
+  const setingTablero = (tab) => {
+    setTablero(tab);
   };
 
-  const changeLIve = (live, x, y) => {
-    setTablero((table) => {
-      const newTable = table;
-      newTable[x][y] = 1;
-      return [...newTable];
-    });
+  const stopOrLive = () => {
+    setLive((l) => !l);
   };
 
   return (
     <>
-      <button type="button" onClick={() => setCount((cuenta) => cuenta + 1)}>
-        {count}
-      </button>
-      <input
-        type="number"
-        ref={numBoxes}
-      />
-      <button type="button" onClick={listenInput}>
-        Aceptar
-      </button>
-      <button type="button">
-        {boxes}
-      </button>
-      <div className="tablero" style={{ gridTemplateColumns: `repeat(${boxes},1fr)` }}>
-        {tablero.map((n, x) => (
-          <div className="column" style={{ gridTemplateRows: `repeat(${boxes},1fr)` }}>
-            {n.map((live, y) => (
-              // eslint-disable-next-line jsx-a11y/click-events-have-key-events
-              <div
-                onClick={() => changeLIve(live, x, y)}
-                className={`box ${live === 1 ? 'live' : 'dead'}`}
-              />
-            ))}
-          </div>
-        ))}
-      </div>
+      <Options />
+      <button type="button" onClick={() => stopOrLive()} />
+      {live ? (
+        <TableLive
+          tablero={tablero}
+          boxes={boxes}
+          setingTablero={setingTablero}
+        />
+      )
+        : (
+          <TableSTOP
+            tablero={tablero}
+            boxes={boxes}
+            setingTablero={setingTablero}
+          />
+        )}
     </>
   );
 }
