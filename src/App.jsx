88d71293b17b4f4a1c5746d65840b023ee97f1c8
Diegoo11 <ynoacamino@gmail.com>
2023-06-11 14:34:07 -0500

    added module options and deleted functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react-hooks/exhaustive-deps */
 import {
-  useState, React, useRef,
+  useState, React,
 } from 'react';
 import './App.css';
 import { createArrays } from './test/util';
@@ -11,36 +11,55 @@ import Options from './Options/Options';
 
 function App() {
   // const [count, setCount] = useState(0);
-  const [boxes, setBoxes] = useState(100);
+  // eslint-disable-next-line no-unused-vars
+  const [boxes, setBoxes] = useState(Math.round(window.screen.width / 28));
   const [tablero, setTablero] = useState(createArrays(boxes));
   const [live, setLive] = useState(false);
+  const [velo, setVelo] = useState(false);
 
   const setingTablero = (tab) => {
-    setTablero(tab);
+    setTablero([...tab]);
   };
 
   const stopOrLive = () => {
     setLive((l) => !l);
   };
 
+  const resetTable = () => {
+    setTablero([...createArrays(boxes)]);
+  };
+
+  const moreVelo = () => {
+    setVelo((v) => !v);
+  };
+
   return (
     <>
-      <Options />
-      <button type="button" onClick={() => stopOrLive()} />
-      {live ? (
-        <TableLive
-          tablero={tablero}
-          boxes={boxes}
-          setingTablero={setingTablero}
-        />
-      )
-        : (
-          <TableSTOP
-            tablero={tablero}
-            boxes={boxes}
-            setingTablero={setingTablero}
-          />
-        )}
+      <Options
+        live={live}
+        stopOrLive={stopOrLive}
+        moreVelo={moreVelo}
+        resetTable={resetTable}
+        velo={velo}
+      />
+
+      {live && (
+      <TableLive
+        tablero={tablero}
+        boxes={boxes}
+        setingTablero={setingTablero}
+        velo={velo}
+      />
+      )}
+
+      {!live && (
+      <TableSTOP
+        tablero={tablero}
+        boxes={boxes}
+        setTablero={setTablero}
+      />
+      )}
+
     </>
   );
 }
