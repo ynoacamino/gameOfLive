c95b6fdd5d14d9538287cc31d5be1416789fee98
Diegoo11 <ynoacamino@gmail.com>
2023-06-11 14:35:14 -0500

    module for control status of table

--- src/Options/Options.jsx
+++ src/Options/Options.jsx
@@ -1,9 +1,25 @@
 import './Options.css';
 import React from 'react';
+import btnPause from '../img/btnPause.png';
+import btnVelocity from '../img/btnVelocity.png';
+import btnReset from '../img/btnReset.png';
+import btnLive from '../img/btnLive.png';
 
-function Options() {
+function Options({
+  live, stopOrLive, moreVelo, resetTable, velo,
+}) {
   return (
-    <div />
+    <div className="Options">
+      <button className="optionsButtons" type="button" onClick={stopOrLive}>
+        <img className="imgButtons" src={live ? btnPause : btnLive} alt="pauseAndLive" />
+      </button>
+      <button className="optionsButtons" type="button" onClick={resetTable} disabled={live}>
+        <img className="imgButtons" src={btnReset} alt="reset" />
+      </button>
+      <button className={`optionsButtons ${velo && 'moreVelo'}`} type="button" onClick={moreVelo} disabled={!live}>
+        <img className="imgButtons" src={btnVelocity} alt="moreVelocity" />
+      </button>
+    </div>
   );
 }
 
